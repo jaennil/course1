f79e035c6e3aa10b0f52be38cf8cd4c48577e378
ssushnost <ssushnost@gmail.com>
2023-07-06 05:21:21 +0300

    feat(admin) better mvc, highlighting

--- src/main/java/Controller/Admin.java
+++ src/main/java/Controller/Admin.java
@@ -1,20 +1,15 @@
 package Controller;
 
 import Other.Person;
-import javafx.application.Platform;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
 import org.example.App;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Admin implements Initializable {
-    private final Model.Registration model = new Model.Registration();
+    private final Model.Admin model = new Model.Admin();
     public Label firstnameEmptyLabel;
     public Label surnameEmptyLabel;
     public TextField firstnameField;
@@ -27,69 +22,50 @@ public class Admin implements Initializable {
     public PasswordField passwordField;
     public Label welcomeLabel;
     public ComboBox<String> roleComboBox;
-    private Person person;
+    public Label roleEmptyLabel;
 
     public void passPerson(Person person) {
-        this.person = person;
+        model.setAuthenticatedUser(person);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        ObservableList<String> roles = FXCollections.observableArrayList("Employee", "Doctor");
-        roleComboBox.setItems(roles);
-        Platform.runLater(() -> welcomeLabel.setText("Welcome, " + person.getWelcomeName()));
+        firstnameField.textProperty().addListener((observable, oldValue, newValue) -> model.setFirstname(newValue));
+        surnameField.textProperty().addListener((observable, oldValue, newValue) -> model.setSurname(newValue));
+        lastnameField.textProperty().addListener((observable, oldValue, newValue) -> model.setLastname(newValue));
+        usernameField.textProperty().addListener((observable, oldValue, newValue) -> model.setUsername(newValue));
+        passwordField.textProperty().addListener((observable, oldValue, newValue) -> model.setPassword(newValue));
+        roleComboBox.valueProperty().addListener((observable, oldValue, newValue) -> model.setRole(newValue));
+
+        firstnameField.styleProperty().bindBidirectional(model.firstnameFieldStyle);
+        surnameField.styleProperty().bindBidirectional(model.surnameFieldStyle);
+        lastnameField.styleProperty().bindBidirectional(model.lastnameFieldStyle);
+        usernameField.styleProperty().bindBidirectional(model.usernameFieldStyle);
+        passwordField.styleProperty().bindBidirectional(model.passwordFieldStyle);
+        roleComboBox.styleProperty().bindBidirectional(model.roleComboBoxStyle);
+
+        firstnameEmptyLabel.visibleProperty().bindBidirectional(model.firstnameEmpty);
+        surnameEmptyLabel.visibleProperty().bindBidirectional(model.surnameEmpty);
+        lastnameEmptyLabel.visibleProperty().bindBidirectional(model.lastnameEmpty);
+        usernameEmptyLabel.visibleProperty().bindBidirectional(model.usernameEmpty);
+        passwordEmptyLabel.visibleProperty().bindBidirectional(model.passwordEmpty);
+        roleEmptyLabel.visibleProperty().bindBidirectional(model.roleEmpty);
+
+        welcomeLabel.textProperty().bindBidirectional(model.welcomeText);
+        roleComboBox.setItems(model.roles);
     }
 
-    public void addEmployee(ActionEvent actionEvent) {
-        String firstname = firstnameField.getText();
-        String surname = surnameField.getText();
-        String lastname = lastnameField.getText();
-        String username = usernameField.getText();
-        String password = passwordField.getText();
-        String role = roleComboBox.getValue();
-        spawnWarningLabels();
-        highlightEmptyFields();
-        if (firstname.isBlank() || surname.isBlank() || lastname.isBlank() || username.isBlank() || password.isBlank() || role == null)
-            return;
-        model.addUser(firstname, surname, lastname, username, password, role);
-        showInformationDialog(firstname, surname, lastname);
-        clearFields();
+    public void addEmployee() {
+        model.setFirstname(firstnameField.getText());
+        model.setSurname(surnameField.getText());
+        model.setLastname(lastnameField.getText());
+        model.setUsername(usernameField.getText());
+        model.setPassword(passwordField.getText());
+        model.setRole(roleComboBox.getValue());
+        model.addUser();
     }
 
-    private void highlightEmptyFields() {
-        firstnameField.setStyle(firstnameField.getText().isBlank() ? "-fx-border-color: red;" : "");
-        surnameField.setStyle(surnameField.getText().isBlank() ? "-fx-border-color: red;" : "");
-        lastnameField.setStyle(lastnameField.getText().isBlank() ? "-fx-border-color: red;" : "");
-        usernameField.setStyle(usernameField.getText().isBlank() ? "-fx-border-color: red;" : "");
-        passwordField.setStyle(passwordField.getText().isBlank() ? "-fx-border-color: red;" : "");
-        roleComboBox.setStyle(roleComboBox.getValue() == null ? "-fx-border-color: red;" : "");
-    }
-
-    private void spawnWarningLabels() {
-        firstnameEmptyLabel.setVisible(firstnameField.getText().isBlank());
-        surnameEmptyLabel.setVisible(surnameField.getText().isBlank());
-        lastnameEmptyLabel.setVisible(lastnameField.getText().isBlank());
-        usernameEmptyLabel.setVisible(usernameField.getText().isBlank());
-        passwordEmptyLabel.setVisible(passwordField.getText().isBlank());
-    }
-
-    private void showInformationDialog(String firstname, String surname, String lastname) {
-        Alert dialog = new Alert(Alert.AlertType.INFORMATION);
-        dialog.setTitle("Adding employee");
-        dialog.setHeaderText("Successfully added employee");
-        dialog.setContentText(surname + " " + firstname + " " + lastname);
-        dialog.showAndWait();
-    }
-    private void clearFields() {
-        firstnameField.clear();
-        surnameField.clear();
-        lastnameField.clear();
-        usernameField.clear();
-        passwordField.clear();
-        roleComboBox.setValue(null);
-    }
-
-    public void logOut(MouseEvent mouseEvent) {
+    public void logOut() {
         try {
             App.setRoot("authorization");
         } catch (IOException e) {
---
+++ src/main/java/Model/Admin.java
@@ -0,0 +1,118 @@
+package Model;
+
+import Other.Database;
+import Other.Hash;
+import Other.Person;
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.Alert;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Admin {
+    public ObservableList<String> roles = FXCollections.observableArrayList("Employee", "Doctor");
+    public StringProperty firstnameFieldStyle = new SimpleStringProperty();
+    public final StringProperty surnameFieldStyle = new SimpleStringProperty();
+    public final StringProperty lastnameFieldStyle = new SimpleStringProperty();
+    public final StringProperty usernameFieldStyle = new SimpleStringProperty();
+    public final StringProperty passwordFieldStyle = new SimpleStringProperty();
+    public final StringProperty roleComboBoxStyle = new SimpleStringProperty();
+    public final StringProperty firstname = new SimpleStringProperty();
+    public final StringProperty surname = new SimpleStringProperty();
+    public final StringProperty lastname = new SimpleStringProperty();
+    public final StringProperty username = new SimpleStringProperty();
+    public final StringProperty password = new SimpleStringProperty();
+    public final StringProperty role = new SimpleStringProperty();
+    public BooleanProperty firstnameEmpty = new SimpleBooleanProperty(false);
+    public BooleanProperty surnameEmpty = new SimpleBooleanProperty(false);
+    public BooleanProperty lastnameEmpty = new SimpleBooleanProperty(false);
+    public BooleanProperty usernameEmpty = new SimpleBooleanProperty(false);
+    public BooleanProperty passwordEmpty = new SimpleBooleanProperty(false);
+    public BooleanProperty roleEmpty = new SimpleBooleanProperty(false);
+    public StringProperty welcomeText = new SimpleStringProperty("Welcome, {firstname} {lastname}");
+    public ObjectProperty<Person> authenticatedUser = new SimpleObjectProperty<>();
+    private final String highlightedStyle = "-fx-border-color: red; -fx-border-width: 2px;";
+    private final String defaultStyle = "";
+
+    public void setFirstname(String string) {
+        this.firstname.set(string);
+        firstnameFieldStyle.set(string.isBlank() ? highlightedStyle : defaultStyle);
+        firstnameEmpty.set(string.isBlank());
+    }
+
+    public void setSurname(String string) {
+        this.surname.set(string);
+        surnameFieldStyle.set(string.isBlank() ? highlightedStyle : defaultStyle);
+        surnameEmpty.set(string.isBlank());
+    }
+
+    public void setLastname(String string) {
+        this.lastname.set(string);
+        lastnameFieldStyle.set(string.isBlank() ? highlightedStyle : defaultStyle);
+        lastnameEmpty.set(string.isBlank());
+    }
+
+    public void setUsername(String string) {
+        this.username.set(string);
+        usernameFieldStyle.set(string.isBlank() ? highlightedStyle : defaultStyle);
+        usernameEmpty.set(string.isBlank());
+    }
+
+    public void setPassword(String string) {
+        this.password.set(string);
+        passwordFieldStyle.set(string.isBlank() ? highlightedStyle : defaultStyle);
+        passwordEmpty.set(string.isBlank());
+    }
+
+    public void setRole(String string) {
+        this.role.set(string);
+        roleComboBoxStyle.set(string == null ? highlightedStyle : defaultStyle);
+        roleEmpty.set(string == null);
+    }
+
+    public void setAuthenticatedUser(Person user) {
+        authenticatedUser.set(user);
+        welcomeText.set("Welcome, " + user.getWelcomeName());
+    }
+
+    public void showInformationDialog() {
+        Alert dialog = new Alert(Alert.AlertType.INFORMATION);
+        dialog.setTitle("Adding employee");
+        dialog.setHeaderText("Successfully added employee");
+        dialog.setContentText(surname + " " + firstname + " " + lastname);
+        dialog.showAndWait();
+    }
+
+    private void nullValues() {
+        firstname.set("");
+        surname.set("");
+        lastname.set("");
+        username.set("");
+        password.set("");
+        role.set("");
+    }
+
+    public void addUser() {
+        if (firstnameEmpty.get() || surnameEmpty.get() || lastnameEmpty.get() || usernameEmpty.get() || passwordEmpty.get() || roleEmpty.get())
+            return;
+        Database database = Database.getInstance();
+        String statement = "insert into people (firstname, surname, lastname, address, phone_number, username, password_hash, role) value (?,?,?,?,?,?,?,?)";
+        try (PreparedStatement preparedStatement = database.getConnection().prepareStatement(statement)) {
+            preparedStatement.setString(1, firstname.get());
+            preparedStatement.setString(2, surname.get());
+            preparedStatement.setString(3, lastname.get());
+            preparedStatement.setString(4, null);
+            preparedStatement.setString(5, null);
+            preparedStatement.setString(6, username.get());
+            preparedStatement.setString(7, Hash.toString(Hash.hash(password.get())));
+            preparedStatement.setString(8, role.get());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        showInformationDialog();
+        nullValues();
+    }
+}
